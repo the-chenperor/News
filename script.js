18f281649af04fbc4ac5316060ba421b744d1775
zachmothner <zachary.mothner@gmail.com>
2015-08-28 14:13:29 +0000

    commit

---
+++ script.js
@@ -0,0 +1,224 @@
+var articles =
+  {
+    "News":
+      [
+        {
+          "title": "Choate Welcomes Spring with 28th Annual Festival AnnualFSpringFest",
+          "author": "Author",
+          "time": "August 25, 2015",
+          "tags": ["News", "Lorem Ipsum"],
+          "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum placerat velit quis augue tincidunt, quis porta velit pharetra. Vestibulum placerat tortor et massa commodo posuere. Quisque nec accumsan nibh. Sed tempor quam ac ultrices laoreet. Nunc interdum, urna sed tempor fringilla, lectus ex lacinia nibh, ut bibendum lorem libero quis.",
+          "images": "none"
+        },
+        {
+          "title": "Choate Names Program in Honor of Andy B. Noel III",
+          "author": "Author",
+          "time": "August 26, 2015",
+          "tags": ["News", "Lorem Ipsum"],
+          "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum placerat velit quis augue tincidunt, quis porta velit pharetra. Vestibulum placerat tortor et massa commodo posuere. Quisque nec accumsan nibh. Sed tempor quam ac ultrices laoreet. Nunc interdum, urna sed tempor fringilla, lectus ex lacinia nibh, ut bibendum lorem libero quis.",
+          "images": "none"
+        },
+        {
+          "title": "Fourth and Fifth Formers to be Integrated in Dorms",
+          "author": "Author",
+          "time": "August 27, 2015",
+          "tags": ["News", "Lorem Ipsum"],
+          "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum placerat velit quis augue tincidunt, quis porta velit pharetra. Vestibulum placerat tortor et massa commodo posuere. Quisque nec accumsan nibh. Sed tempor quam ac ultrices laoreet. Nunc interdum, urna sed tempor fringilla, lectus ex lacinia nibh, ut bibendum lorem libero quis.",
+          "images": "none"
+        }
+      ],
+    "Features":
+      [
+        {
+          "title": "Take a Walk on the Wild Side",
+          "author": "Author",
+          "time": "August 25, 2015",
+          "tags": ["Opinions", "Lorem Ipsum"],
+          "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum placerat velit quis augue tincidunt, quis porta velit pharetra. Vestibulum placerat tortor et massa commodo posuere. Quisque nec accumsan nibh. Sed tempor quam ac ultrices laoreet. Nunc interdum, urna sed tempor fringilla, lectus ex lacinia nibh, ut bibendum lorem libero quis.",
+          "images": "none"
+        },
+        {
+          "title": "Q&A Around Campus: Mr. Bobby Dixon",
+          "author": "Author",
+          "time": "August 26, 2015",
+          "tags": ["Opinions", "Lorem Ipsum"],
+          "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum placerat velit quis augue tincidunt, quis porta velit pharetra. Vestibulum placerat tortor et massa commodo posuere. Quisque nec accumsan nibh. Sed tempor quam ac ultrices laoreet. Nunc interdum, urna sed tempor fringilla, lectus ex lacinia nibh, ut bibendum lorem libero quis.",
+          "images": "none"
+        },
+        {
+          "title": "Choate Entrepreneurs Develop C-Harmony",
+          "author": "Author",
+          "time": "August 27, 2015",
+          "tags": ["Opinions", "Lorem Ipsum"],
+          "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum placerat velit quis augue tincidunt, quis porta velit pharetra. Vestibulum placerat tortor et massa commodo posuere. Quisque nec accumsan nibh. Sed tempor quam ac ultrices laoreet. Nunc interdum, urna sed tempor fringilla, lectus ex lacinia nibh, ut bibendum lorem libero quis.",
+          "images": "none"
+        }
+      ],
+    "Opinions-Campus-Issues":
+      [
+        {
+          "title": "Title1",
+          "author": "Author",
+          "time": "August 25, 2015",
+          "tags": ["Opinions", "Campus Issues", "Lorem Ipsum"],
+          "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum placerat velit quis augue tincidunt, quis porta velit pharetra. Vestibulum placerat tortor et massa commodo posuere. Quisque nec accumsan nibh. Sed tempor quam ac ultrices laoreet. Nunc interdum, urna sed tempor fringilla, lectus ex lacinia nibh, ut bibendum lorem libero quis.",
+          "images": "none"
+        },
+        {
+          "title": "Title2",
+          "author": "Author",
+          "time": "August 26, 2015",
+          "tags": ["Opinions", "Campus Issues", "Lorem Ipsum"],
+          "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum placerat velit quis augue tincidunt, quis porta velit pharetra. Vestibulum placerat tortor et massa commodo posuere. Quisque nec accumsan nibh. Sed tempor quam ac ultrices laoreet. Nunc interdum, urna sed tempor fringilla, lectus ex lacinia nibh, ut bibendum lorem libero quis.",
+          "images": "none"
+        },
+        {
+          "title": "Title3",
+          "author": "Author",
+          "time": "August 27, 2015",
+          "tags": ["Opinions", "Campus Issues", "Lorem Ipsum"],
+          "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum placerat velit quis augue tincidunt, quis porta velit pharetra. Vestibulum placerat tortor et massa commodo posuere. Quisque nec accumsan nibh. Sed tempor quam ac ultrices laoreet. Nunc interdum, urna sed tempor fringilla, lectus ex lacinia nibh, ut bibendum lorem libero quis.",
+          "images": "none"
+        }
+      ],
+    "Opinions-Nation-World":
+    [
+        {
+          "title": "Title1",
+          "author": "Author",
+          "time": "August 25, 2015",
+          "tags": ["Features", "Lorem Ipsum"],
+          "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum placerat velit quis augue tincidunt, quis porta velit pharetra. Vestibulum placerat tortor et massa commodo posuere. Quisque nec accumsan nibh. Sed tempor quam ac ultrices laoreet. Nunc interdum, urna sed tempor fringilla, lectus ex lacinia nibh, ut bibendum lorem libero quis.",
+          "images": "none"
+        },
+        {
+          "title": "Title2",
+          "author": "Author",
+          "time": "August 26, 2015",
+          "tags": ["Features", "Lorem Ipsum"],
+          "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum placerat velit quis augue tincidunt, quis porta velit pharetra. Vestibulum placerat tortor et massa commodo posuere. Quisque nec accumsan nibh. Sed tempor quam ac ultrices laoreet. Nunc interdum, urna sed tempor fringilla, lectus ex lacinia nibh, ut bibendum lorem libero quis.",
+          "images": "none"
+        },
+        {
+          "title": "Title3",
+          "author": "Author",
+          "time": "August 27, 2015",
+          "tags": ["Features", "Lorem Ipsum"],
+          "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum placerat velit quis augue tincidunt, quis porta velit pharetra. Vestibulum placerat tortor et massa commodo posuere. Quisque nec accumsan nibh. Sed tempor quam ac ultrices laoreet. Nunc interdum, urna sed tempor fringilla, lectus ex lacinia nibh, ut bibendum lorem libero quis.",
+          "images": "none"
+        }
+      ],
+    "Arts-Leisure":
+    [
+        {
+          "title": "Title1",
+          "author": "Author",
+          "time": "August 25, 2015",
+          "tags": ["Arts & Leisure", "Lorem Ipsum"],
+          "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum placerat velit quis augue tincidunt, quis porta velit pharetra. Vestibulum placerat tortor et massa commodo posuere. Quisque nec accumsan nibh. Sed tempor quam ac ultrices laoreet. Nunc interdum, urna sed tempor fringilla, lectus ex lacinia nibh, ut bibendum lorem libero quis.",
+          "images": "none"
+        },
+        {
+          "title": "Title2",
+          "author": "Author",
+          "time": "August 26, 2015",
+          "tags": ["Arts & Leisure", "Lorem Ipsum"],
+          "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum placerat velit quis augue tincidunt, quis porta velit pharetra. Vestibulum placerat tortor et massa commodo posuere. Quisque nec accumsan nibh. Sed tempor quam ac ultrices laoreet. Nunc interdum, urna sed tempor fringilla, lectus ex lacinia nibh, ut bibendum lorem libero quis.",
+          "images": "none"
+        },
+        {
+          "title": "Title3",
+          "author": "Author",
+          "time": "August 27, 2015",
+          "tags": ["Arts & Lesiure", "Lorem Ipsum"],
+          "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum placerat velit quis augue tincidunt, quis porta velit pharetra. Vestibulum placerat tortor et massa commodo posuere. Quisque nec accumsan nibh. Sed tempor quam ac ultrices laoreet. Nunc interdum, urna sed tempor fringilla, lectus ex lacinia nibh, ut bibendum lorem libero quis.",
+          "images": "none"
+        }
+      ],
+    "Sports":
+      [
+        {
+          "title": "Title1",
+          "author": "Author",
+          "time": "August 25, 2015",
+          "tags": ["Sports", "Lorem Ipsum"],
+          "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum placerat velit quis augue tincidunt, quis porta velit pharetra. Vestibulum placerat tortor et massa commodo posuere. Quisque nec accumsan nibh. Sed tempor quam ac ultrices laoreet. Nunc interdum, urna sed tempor fringilla, lectus ex lacinia nibh, ut bibendum lorem libero quis.",
+          "images": "none"
+        },
+        {
+          "title": "Title2",
+          "author": "Author",
+          "time": "August 26, 2015",
+          "tags": ["Sports", "Lorem Ipsum"],
+          "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum placerat velit quis augue tincidunt, quis porta velit pharetra. Vestibulum placerat tortor et massa commodo posuere. Quisque nec accumsan nibh. Sed tempor quam ac ultrices laoreet. Nunc interdum, urna sed tempor fringilla, lectus ex lacinia nibh, ut bibendum lorem libero quis.",
+          "images": "none"
+        },
+        {
+          "title": "Title3",
+          "author": "Author",
+          "time": "August 27, 2015",
+          "tags": ["Sports", "Lorem Ipsum"],
+          "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum placerat velit quis augue tincidunt, quis porta velit pharetra. Vestibulum placerat tortor et massa commodo posuere. Quisque nec accumsan nibh. Sed tempor quam ac ultrices laoreet. Nunc interdum, urna sed tempor fringilla, lectus ex lacinia nibh, ut bibendum lorem libero quis.",
+          "images": "none"
+        }
+      ]
+  }
+
+var timer;
+var mouseIn = false;
+
+$(document).ready(function() {
+
+  function displayAll() {
+    var i = 0;
+    var squares = ["#News", "#Features", "#Opinions-Campus-Issues", "#Opinions-Nation-World", "#Arts-Leisure", "#Sports", "#Popular", "#Recent", "#Commented"];
+    for(i = 0; i < squares.length; i++) {
+      $(squares[i]).append("<div class='titles' style='font-weight:300;visibility:hidden;position:relative;bottom:120%;margin:10px;border:1px solid white;'><a class='headline' href='index.html' style='color:white;text-decoration:none;line-height:1;'></a><br><p class='author' style='color:white;'></p><p class='date' style='color:white;'></p></div><div class='circles' style='visibility:hidden;position:relative;bottom:120%;margin-left:auto;margin-right:auto;height:20px;width:100px;'><div class='c0' style='margin:5px;padding:5px;height:10px;width:10px;border-radius:25px;border:1px solid white;background-color:white;'></div><div class='c1' style='margin:5px;padding:5px;height:10px;width:10px;border-radius:25px;border:1px solid white;background-color:white;'></div><div class='c2' style='margin:5px;padding:5px;height:10px;width:10px;border-radius:25px;border:1px solid white;background-color:white;'></div></div>");
+    }
+  }
+  displayAll();
+
+  // function (category, number)
+  function display(cat, num){
+    $(".c0").css("opacity","0.5");
+    $(".c1").css("opacity","0.5");
+    $(".c2").css("opacity","0.5");
+    $(".titles").html("");
+    $(".titles").html("<a href='index.html' style='color:white;text-decoration:none;line-height:1;'>" + articles[cat][num]["title"] + "</a><br><p style='color:white;'>" + articles[cat][num]["author"] + "</p><p style='color:white;'>" + articles[cat][num]["time"] +"</p>");
+    var c = ".c" + num;
+    $(c).css("opacity","1");
+    if(num+1 < articles[cat].length) {
+      timer = setTimeout(display, 3000, cat, num+1);
+    }
+    else {
+      timer = setTimeout(display, 3000, cat, 0);
+    }
+  }
+
+
+  $(".square").hover(function() {
+//     $(this).append("<div class='titles' style='position:relative;bottom:105%;margin:10px;border:1px solid white;'></div>");
+//     $(this).append("<div class='circles' style='position:relative;bottom:105%;margin-left:auto;margin-right:auto;height:20px;width:100px;'></div>");
+//     $(".circles").append("<div class='c0' style='margin:5px;padding:5px;height:10px;width:10px;border-radius:25px;border:1px solid white;background-color:white;'></div>");
+//     $(".circles").append("<div class='c1' style='margin:5px;padding:5px;height:10px;width:10px;border-radius:25px;border:1px solid white;background-color:white;'></div>");
+//     $(".circles").append("<div class='c2' style='margin:5px;padding:5px;height:10px;width:10px;border-radius:25px;border:1px solid white;background-color:white;'></div>");
+    if(!mouseIn) {
+      mouseIn = true;
+      display($(this)[0].id, 0);
+      var id = "#" + $(this)[0].id;
+      $(id + " .titles").css("visibility", "visible");
+      $(id + " .circles").css("visibility", "visible");
+    }
+  },
+  function() {
+    mouseIn = false;
+    var id = "#" + $(this)[0].id;
+    $(id + " .titles").css("visibility", "hidden");
+    $(id + " .circles").css("visibility", "hidden");
+    clearTimeout(timer);
+//     $(".titles").html("");
+//     $(".titles").remove();
+//     $(".circles").html("");
+//     $(".circles").remove();
+  });
+});
